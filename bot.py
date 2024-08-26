f54b176ad679ed32aa2be79fad5885fe26e3d9de
emily pancakes <awesomepopcornchi@gmail.com>
2024-08-26 12:10:39 -0400

    Added make_ticket

--- bot.py
+++ bot.py
@@ -18,12 +18,15 @@ except:
 # start up bot and set intents
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 
 client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user}')
+    global guild
+    guild = client.get_guild(0) # to be filled in later
 
 # test command
 @client.event
@@ -32,6 +35,22 @@ async def on_message(message):
         return
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+
+
+# on member join
+@client.event
+async def on_member_join(member):
+    # await chat.send(f'{member} hath joined the fray')
+    pass
+
+# make channel
+async def make_ticket(member):
+    channel = await guild.create_text_channel('test',
+                                              overwrites={
+                                                  guild.default_role: discord.PermissionOverwrite(read_messages=False),
+                                                  guild.me: discord.PermissionOverwrite(read_messages=True),
+                                                  member: discord.PermissionOverwrite(read_messages=True)
+                                              })
 
 # truly start bot
 client.run(token)
